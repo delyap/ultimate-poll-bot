90fd8a3a0a2ed266afca3b0a1f3dcc504d8aa06e
Frederik Wille <frederik@wille.hamburg>
2019-08-25 11:13:09 +0200

    implement basic index request

--- poll_api/resources/poll.py
+++ poll_api/resources/poll.py
@@ -1,16 +1,28 @@
-from flask_restful import Resource
+from flask_restful import Resource, fields, marshal_with
 from pollbot.models import Poll
 from pollbot.db import get_session
 
+resource_fields = {
+    'uuid': fields.String,
+    'name': fields.String,
+    'description': fields.String,
+    'locale': fields.String,
+    'poll_type': fields.String,
+    'anonymous': fields.Boolean,
+    'results_visible': fields.Boolean,
+    'due_date': fields.DateTime,
+    'created': fields.Boolean,
+    'closed': fields.Boolean
+}
+
 class PollResource(Resource):
+    @marshal_with(resource_fields)
     def get(self):
-        # print("debug")
-        # return {'asd':'asd'}
         session = get_session()
         polls = session.query(Poll) \
             .filter(Poll.created.is_(True)) \
             .filter(Poll.closed.is_(False)) \
             .all()
             # .filter(Poll.user == user) TODO
-        print(polls[0])
-        return polls[0]
+        print(polls)
+        return polls
